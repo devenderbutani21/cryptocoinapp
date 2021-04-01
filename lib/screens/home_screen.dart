4fe51b84a94f48679c09c3dfc9e0c1703a15a5da
devenderbutani21 <devenderbutani21@gmail.com>
2021-04-01 18:36:08 -0400

    UI setup

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cryptocoinapp/models/cryptocoin_model.dart';
 import 'package:flutter/material.dart';
 import 'package:cryptocoinapp/services/crypto_service.dart';
 
@@ -7,12 +8,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Future<CryptoCoinModel> cryptoCoinData;
 
   @override
   void initState() {
-    CryptoService().fetchCoinValues();
+    cryptoCoinData = CryptoService().fetchCoinValues();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +23,62 @@ class _HomeScreenState extends State<HomeScreen> {
         height: double.maxFinite,
         width: double.maxFinite,
         color: Colors.blue,
+        child: FutureBuilder<CryptoCoinModel>(
+            future: CryptoService().fetchCoinValues(),
+            builder: (BuildContext context,
+                AsyncSnapshot<CryptoCoinModel> snapshot) {
+              if (snapshot.hasData) {
+                CryptoCoinModel _cryptocoinData = snapshot.data;
+                return Container(
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Center(
+                          child: Text(
+                            "${_cryptocoinData.data[0].symbol}",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 40,
+                      ),
+                      Text(
+                        "${_cryptocoinData.data[0].name}",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 40,
+                      ),
+                      Text(
+                        "${_cryptocoinData.data[0].priceUsd}",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              } else if (!snapshot.hasData) {
+                return Container();
+              }
+            }),
       ),
     );
   }
--- lib/services/crypto_service.dart
+++ lib/services/crypto_service.dart
@@ -7,7 +7,6 @@ class CryptoService{
   Future<CryptoCoinModel> fetchCoinValues() async {
     final response = await http.get('https://api.coinlore.net/api/tickers/');
     if(response.statusCode == 200 ) {
-      print(response.body);
       return CryptoCoinModel.fromJson(jsonDecode(response.body));
     } else {
       throw Exception("Crypto Coins Values not found");
