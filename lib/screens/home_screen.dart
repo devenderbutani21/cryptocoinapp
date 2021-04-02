277abdd289d71485772dafccdf749dfa05eb9eee
devenderbutani21 <devenderbutani21@gmail.com>
2021-04-02 19:00:29 -0400

    Home Screen UI changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -19,6 +19,17 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Cryptocurrency",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 28,
+            fontFamily: "Roboto",
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
       body: Container(
         height: double.maxFinite,
         width: double.maxFinite,
@@ -29,51 +40,105 @@ class _HomeScreenState extends State<HomeScreen> {
                 AsyncSnapshot<CryptoCoinModel> snapshot) {
               if (snapshot.hasData) {
                 CryptoCoinModel _cryptocoinData = snapshot.data;
-                return Container(
-                  child: Row(
-                    children: [
-                      Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          shape: BoxShape.circle,
+                return ListView.builder(
+                  itemCount: _cryptocoinData.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Column(
+                      children: [
+                        SizedBox(
+                          height: 10,
                         ),
-                        child: Center(
-                          child: Text(
-                            "${_cryptocoinData.data[0].symbol}",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
+                        Container(
+                          width: 360,
+                          height: 90,
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: 360,
+                                height: 90,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(16),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Color(0x19000000),
+                                      blurRadius: 10,
+                                      offset: Offset(4, 4),
+                                    ),
+                                  ],
+                                  color: Colors.white,
+                                ),
+                                padding: const EdgeInsets.only(
+                                  left: 10,
+                                  right: 20,
+                                ),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                      width: 60,
+                                      height: 60,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Color(0xffc4c4c4),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          "${_cryptocoinData.data[index].symbol}",
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 16,
+                                            fontFamily: "Roboto",
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Expanded(
+                                      flex: 6,
+                                      child: Text(
+                                        "${_cryptocoinData.data[index].name}",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 24,
+                                          fontFamily: "Roboto",
+                                          fontWeight: FontWeight.w700,
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      flex: 4,
+                                      child: Text(
+                                        "${double.parse(_cryptocoinData.data[index].priceUsd).toDouble()}",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 24,
+                                          fontFamily: "Roboto",
+                                          fontWeight: FontWeight.w700,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        width: 40,
-                      ),
-                      Text(
-                        "${_cryptocoinData.data[0].name}",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
+                        SizedBox(
+                          height: 10,
                         ),
-                      ),
-                      SizedBox(
-                        width: 40,
-                      ),
-                      Text(
-                        "${_cryptocoinData.data[0].priceUsd}",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
+                      ],
+                    );
+                  },
                 );
               } else if (!snapshot.hasData) {
                 return Container();
